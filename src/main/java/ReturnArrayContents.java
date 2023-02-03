e560cd0ecd1828b05ef6db2902e6f442eb3dc68b
jribezzo <Jesseribezzo@gmail.com>
2023-02-03 05:58:28 +0000

    done

--- src/main/java/ReturnArrayContents.java
+++ src/main/java/ReturnArrayContents.java
@@ -1,3 +1,5 @@
+
+
 public class ReturnArrayContents {
     /**
      * Return a string with an array's contents, formatted with spaces.
@@ -27,7 +29,17 @@ public class ReturnArrayContents {
      * @param nums the array to be manipulated.
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
-    public String arrayToString(int[] nums){
-        return "";
+    public String arrayToString(int[] nums)
+    {
+        String str = "";
+        int num;
+        for (int i = 0; i < nums.length; i++)
+        {
+            num = nums[i];
+            str += num + " ";
+
+        }
+        System.out.println(str);
+        return str;
     }
 }
